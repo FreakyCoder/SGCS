82f66532296861478bf337a97038724c5da6ef1b
Kaloyan Dimitrov <kaloyankd.06@gmail.com>
2022-04-10 18:01:47 +0300

    Made jpackage tool work.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,5 @@
 module com.everfreaky.sgcs {
     requires javafx.controls;
-    requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.everfreaky.sgcs to javafx.fxml;
-    exports com.everfreaky.sgcs;
+    exports com.everfreaky.sgcs to javafx.graphics;
 }
